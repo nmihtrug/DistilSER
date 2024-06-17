f488583c992ef7deeecc0417810880ec3c981df0
TrungAzieL <trung.nguyenminh7804@gmail.com>
2024-06-18 03:28:22 +0700

    fix: dataloader

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -172,9 +172,6 @@ class BaseDataset(Dataset):
         )
         label = self.__plabel__(label)
 
-        if self.data_mode == 'test.pkl':
-            return input_text, input_audio, label
-            
         teacher_input_text = (
             self.list_teacher_encode_text_data[index]
             if self.teacher_encode_data
